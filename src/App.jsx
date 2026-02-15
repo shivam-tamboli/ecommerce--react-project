86eb9de2fbfd2628c4d902d4963dc75ee36db36d
Shivam <shivamtamboli66@gmail.com>
2026-02-15 23:11:23 +0530

    add Routes with path

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,16 @@
-
-import { Link, Route, Routes } from 'react-router-dom';
 import './App.css'
 
-function HomePage() {
-  return<h1>Home Page</h1>
-}
-
-function AboutPage() {
-  return<h1>About Page</h1>
-}
 
 function App() {
-  return (
-    <div>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-      </nav>
-      <Routes> 
-        <Route path='/' element={<HomePage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="*" element={<h1>404 Not Found</h1>} />
+	return (
+		<div className='app'>
+      <Routes>
+        <Route  path="/" />
+        <Route  path="/auth" />
+        <Route  path="/checkout" />
       </Routes>
-      <div>Footer</div>
-    </div>
-
-  )
+      </div>
+	);
 }
 
 export default App;
