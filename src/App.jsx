4b43a642c11d1c352d0deffcc9a93ab622d4e5e3
Shivam <shivamtamboli66@gmail.com>
2026-02-15 23:52:38 +0530

    create Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import './App.css'
 import Home from './pages/Home'
 import Auth from './pages/Auth'
 import Checkout from './pages/Checkout'
+import Navbar from './components/Navbar'
 import { Routes, Route } from 'react-router-dom'
 
 
 function App() {
 	return (
 		<div className='app'>
+      <Navbar />
       <Routes>
         <Route  path="/" element={<Home />}/>
         <Route  path="/auth" element={<Auth />} />
