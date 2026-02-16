68e1ccade056447f9ea8f47984a6f37c6d6d78db
Shivam <shivamtamboli66@gmail.com>
2026-02-16 19:38:56 +0530

    use hooks to update states of user between login and signup and add button

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,19 +1,48 @@
+import { useState } from "react";
+
 export default function Auth() {
-    return <div className="page">
-        <div className="container">
-            <div className="auth-container">
-                <h1 className="page-title">Sign Up</h1>
-                <form className="auth-form">
-                   <div>
-                    <label className="form-label" htmlFor="email">Email</label>
-                    <input className="form-input" type="email" id="email"/>
-                    </div> 
-                    <div>
-                    <label className="form-label" htmlFor="password">Password</label>
-                    <input className="form-input" type="password" id="password"/>
-                    </div> 
-                </form>
+    const [mode, setMode] = useState("signup");
+    return (
+        <div className="page">
+            <div className="container">
+                <div className="auth-container">
+                    <h1 className="page-title"> 
+                        {mode === "signup" ? "Sign Up" : "Login"}
+                    </h1>
+                    <form className="auth-form">
+                       <div>
+                            <label className="form-label" htmlFor="email">Email</label>
+                            <input className="form-input" type="email" id="email"/>
+                        </div> 
+                        <div>
+                            <label className="form-label" htmlFor="password">Password</label>
+                            <input className="form-input" type="password" id="password"/>
+                        </div> 
+
+                        <button type="submit" className="btn btn-primary btn-large">
+                            {mode === "signup" ? "Sign Up" : "Login"}
+                        </button>
+                    </form>
+
+                    <div className="auth-switch">
+                        {mode === "signup" ? (
+                            <p>
+                                Already have an account?{" "}
+                                <span className="auth-link" onClick={() => setMode("login")}>
+                                    Login
+                                </span>
+                            </p>
+                        ) : (
+                            <p>
+                                Don't have an account?{" "}
+                                <span className="auth-link" onClick={() => setMode("signup")}>
+                                    Sign Up
+                                </span>
+                            </p>
+                        )}
+                    </div>
+                </div>
             </div>
         </div>
-    </div>;
-} 
+    );
+}
