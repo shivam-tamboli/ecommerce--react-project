a8555aaaaf93870e5930fee5d8e0448c73f7f00c
Shivam <shivamtamboli66@gmail.com>
2026-02-16 19:10:27 +0530

    take input email and password at signup page

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,3 +1,19 @@
 export default function Auth() {
-    return <div>Auth Page</div>;
-}
+    return <div className="page">
+        <div className="container">
+            <div className="auth-container">
+                <h1 className="page-title">Sign Up</h1>
+                <form className="auth-form">
+                   <div>
+                    <label className="form-label" htmlFor="email">Email</label>
+                    <input className="form-input" type="email" id="email"/>
+                    </div> 
+                    <div>
+                    <label className="form-label" htmlFor="password">Password</label>
+                    <input className="form-input" type="password" id="password"/>
+                    </div> 
+                </form>
+            </div>
+        </div>
+    </div>;
+} 
